acb52099d18bbe1c3ede613ca995d99b242c355c
Huseyin Sencan <hsencan03@gmail.com>
2024-12-07 18:01:33 +0300

    fix(code): fix package folder structure

---
+++ Runtime/Interfaces/IStateMachineHandler.cs
@@ -0,0 +1,11 @@
+namespace FSM.Interfaces
+{
+    public interface IStateMachineHandler<T> where T : IBaseStateData
+    {
+        void AddState(IState<T> state, T stateData);
+
+        void RemoveState();
+
+        void UpdateStates();
+    }
+}
